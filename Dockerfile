600c0c3b80523899be7b0cee556b4f6641cc3a3e
sumair-aslam09 <sumairaslam83@gmail.com>
2023-06-23 18:52:57 +0530

    compose

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
 # Use the official Nginx base image
 FROM nginx
 
+# Remove the default Nginx configuration file
+RUN rm /etc/nginx/conf.d/default.conf
+
+# Copy your custom Nginx configuration file
+COPY nginx.conf /etc/nginx/conf.d/
+
 # Copy your static files to the appropriate directory
 COPY index.html /usr/share/nginx/html/
 
--- check-connection.sh
+++ check-connection.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
-
+NETWORK=sumair_mynetwork
+CON1=sumair_db_1
+CON2=sumair_web_1
 # Check if both containers exist in the network
-if docker network inspect nginx_mynetwork | grep -e "nginx_nginx_1" -e "nginx_mysql_1" >/dev/null 2>&1; then
-    echo "Connection established. Both containers exist in the network."
+if docker network inspect "$NETWORK" | grep -e "$CON1" -e "CON2" >/dev/null 2>&1; then
+    echo "Connection is established.Containers $CON1 and $CON2  exist in the network $NETWORK."
 else
-    echo "Connection not established. One or both containers do not exist in the network."
+    echo "Connection is not established. One or both containers do not exist in the network."
 fi
